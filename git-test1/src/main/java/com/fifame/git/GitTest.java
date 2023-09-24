e577fb13f2f7e2789364c3c304ea4a8e3857b738
fifame <fifame@qq.com>
2023-09-24 15:50:49 +0800

    second commit

---
+++ git-test1/src/main/java/com/fifame/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.fifame.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author fifame
+ * @DATE 2023/09/24 15:47
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
+
