51d4db4f2bd10ce3baf67e40fdc07116a0aedb09
fifame <fifame@qq.com>
2023-09-24 16:39:17 +0800

    hot-fix first push1

--- git-test1/src/main/java/com/fifame/git/GitTest.java
+++ git-test1/src/main/java/com/fifame/git/GitTest.java
@@ -18,6 +18,7 @@ public class GitTest {
         System.out.println("master-hello1341");
         System.out.println("master-hello1341");
         System.out.println("master-hello1341");
+        System.out.println("test - test");
     }
 }
 
